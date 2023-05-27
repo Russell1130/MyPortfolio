41173de92f72b9f8b6358009a004df1d343dbba9
Russell1130 <devlord0625@gmail.com>
2023-05-27 02:22:32 -0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,157 @@
+import p1 from "./assets/project1.jpeg";
+import p2 from "./assets/project2.png";
+import p3 from "./assets/project3.png";
+import p4 from "./assets/project4.png";
+import p5 from "./assets/project5.png";
+import p6 from "./assets/project6.png";
+import p7 from "./assets/project7.png";
+import p8 from "./assets/project8.png";
+import p9 from "./assets/project9.png";
+import p10 from "./assets/project10.png";
+
+export const skills_list = [
+  { path : "/src/assets/SK1.svg" },
+  { path : "/src/assets/SK2.svg" },
+  { path : "/src/assets/SK3.svg" },
+  { path : "/src/assets/SK4.svg" },
+  { path : "/src/assets/SK5.svg" },
+  { path : "/src/assets/SK6.svg" },
+  { path : "/src/assets/SK7.svg" },
+  { path : "/src/assets/SK8.svg" },
+  { path : "/src/assets/SK9.svg" },
+  { path : "/src/assets/SK10.svg" },
+  { path : "/src/assets/SK11.svg" },
+  { path : "/src/assets/SK12.svg" },
+  { path : "/src/assets/SK13.svg" },
+  { path : "/src/assets/SK14.svg" },
+  { path : "/src/assets/SK15.svg" },
+  { path : "/src/assets/SK16.svg" },
+  { path : "/src/assets/SK17.svg" },
+  { path : "/src/assets/SK18.svg" },
+  { path : "/src/assets/SK19.svg" },
+  { path : "/src/assets/SK20.svg" },
+  { path : "/src/assets/SK21.svg" },
+  { path : "/src/assets/SK22.svg" },
+  { path : "/src/assets/SK23.svg" },
+  { path : "/src/assets/SK24.svg" },
+  { path : "/src/assets/SK25.svg" },
+  { path : "/src/assets/SK26.svg" },
+  { path : "/src/assets/SK27.svg" },
+  { path : "/src/assets/SK28.svg" },
+  { path : "/src/assets/SK29.svg" },
+  { path : "/src/assets/SK30.svg" },
+  { path : "/src/assets/SK31.svg" },
+  { path : "/src/assets/SK32.svg" },
+  { path : "/src/assets/SK33.svg" },
+  { path : "/src/assets/SK34.svg" },
+  { path : "/src/assets/SK35.svg" },
+  { path : "/src/assets/SK36.svg" },
+  { path : "/src/assets/SK37.svg" },
+  { path : "/src/assets/SK38.svg" },
+  { path : "/src/assets/SK39.svg" },
+  { path : "/src/assets/SK40.svg" },
+  { path : "/src/assets/SK41.svg" },
+  { path : "/src/assets/SK42.svg" },
+  { path : "/src/assets/SK43.svg" },
+]
+
+export const filters = [
+  {
+    name: "All",
+    id: "mine",
+  },
+  {
+    name: "FullStack",
+    id: "fullstack",
+  },
+  {
+    name: "Blockchain",
+    id: "Crypto",
+  },
+];
+
+export const projects = [
+  {
+    name: "Movie searching App using react,redux,tailwind and TMDB API",
+    image: p1,
+    tags: ["mine", "fullstack", "react", "MDB"],
+    live_url: "https://movie-flix-ashy.vercel.app/",
+  },
+  {
+    name      : "React Quiz App",
+    image     : p2,
+    tags      : ["mine", "fullstack", "react","javascript","recent"],
+    live_url  : "https://react-quiz-liart.vercel.app/",
+  },
+  {
+    name      : "Geneis Vision",
+    image     : p3,
+    tags      : ["mine", "fullstack", "react","materialui"],
+    live_url  : "https://genesis.vision/",
+  },
+  {
+    name      : "UnityMeta",
+    image     : p4,
+    tags      : ["mine", "fullstack", "vue","tailwind"],
+    live_url  : "https://www.unitymeta.finance/",
+  },
+  { 
+    name      : "Ocado Supermaket",
+    image     : p5,
+    tags      : ["mine", "fullstack", "Shopify"],
+    live_url  : "https://www.ocado.com",
+  },
+  {
+    name      : "Nether NFT",
+    image     : p6,
+    tags      : ["mine", "Crypto", "NFT"],
+    live_url  : "https://nethernft.io",
+  }, 
+  {
+    name      : "MM Finance",
+    image     : p7,
+    tags      : ["mine", "Crypto", "Dex", "Defi"],
+    live_url  : "https://polymm.finance",
+  },
+  { 
+    name      : "Defibids",
+    image     : p8,
+    tags      : ["mine", "Crypto", "Defi"],
+    live_url  : "https://defibids.com",
+  },
+  { 
+    name      : "Shirtum",
+    image     : p9,
+    tags      : ["mine", "Crypto", "NFT"],
+    live_url  : "https://shirtum.com/",
+  },
+  {  
+    name      : "Warden2.0",
+    image     : p10,
+    tags      : ["mine", "Crypto", "Dex"],
+    live_url  : "https://wardenswap.finance/",
+  },
+];
+
+export const achievements = [
+  {
+    id: 1,
+    year: 30,
+    description: "SUCCESSFULLY<br />COMPLETED<br />PROJECTS",
+  },
+  {
+    id: 2,
+    year: 9,
+    description: "YEARS OF<br />EXPERIENCE",
+  },
+  {
+    id: 3,
+    year: 20,
+    description: "TECHNOLOGIES",
+  },
+  {
+    id: 4,
+    year: 50,
+    description: "COMMUNITY<br />POSTS",
+  },
+];
